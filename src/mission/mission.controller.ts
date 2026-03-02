0b1b97eea652d12cb69a77a2f5011ca5d78aaaee
Mei-Yoko <keypongphob@gmail.com>
2026-03-02 19:20:10 +0700

    dadadada

--- src/mission/mission.controller.ts
+++ src/mission/mission.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
-import { MissionService } from './mission.service';
+import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
+import { MissionService } from "./mission.service";
 
-@Controller('mission')
+@Controller("missions")
 export class MissionController {
   constructor(private readonly missionService: MissionService) {}
 
-  @Get('summary')
-  getMissionSummary() {
+  @Get()
+  getMissions() {
+    return this.missionService.findAll();
+  }
+
+  @Get("summary")
+  getSummary() {
     return this.missionService.getSummary();
+  }
+
+  @Get(":id")
+  getMissionById(
+    @Param("id") id: string,
+    @Query("clearance") clearance?: string,
+  ) {
+    return this.missionService.findOne(id, clearance);
+  }
+
+  @Post()
+  createMission(
+    @Body()
+    mission: {
+      codename: string;
+      targetName: string;
+      riskLevel: string;
+      startDate: string;
+      id: string;
+      status: string;
+      endDate: string;
+    },
+  ) {
+    return this.missionService.create(mission);
   }
 }
--- src/mission/mission.service.ts
+++ src/mission/mission.service.ts
@@ -1,23 +1,76 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { IMission } from "./mission.interface";
+import * as fs from "fs";
 
 @Injectable()
 export class MissionService {
-  private readonly missions = [
-    { id: 1, codename: 'OPERATION_STORM', status: 'ACTIVE' },
-    { id: 2, codename: 'SILENT_SNAKE', status: 'COMPLETED' },
-    { id: 3, codename: 'RED_DAWN', status: 'FAILED' },
-    { id: 4, codename: 'BLACKOUT', status: 'ACTIVE' },
-    { id: 5, codename: 'ECHO_FALLS', status: 'COMPLETED' },
-    { id: 6, codename: 'GHOST_RIDER', status: 'COMPLETED' },
+  private readonly mission = [
+    { id: 1, codename: "OPERATION_STORM", status: "ACTIVE" },
+    { id: 2, codename: "SILENT_SNAKE", status: "COMPLETED" },
+    { id: 3, codename: "RED_DAWN", status: "FAILED" },
+    { id: 4, codename: "BLACKOUT", status: "ACTIVE" },
+    { id: 5, codename: "ECHO_FALLS", status: "COMPLETED" },
+    { id: 6, codename: "GHOST_RIDER", status: "COMPLETED" },
   ];
 
   getSummary() {
-    return this.missions.reduce(
-      (summary, mission) => {
-        summary[mission.status] = (summary[mission.status] || 0) + 1;
-        return summary;
-      },
-      {} as Record<string, number>,
-    );
+    const res = {
+      ACTIVE: 0,
+      COMPLETED: 0,
+      FAILED: 0,
+    };
+    for (let i = 0; i < this.mission.length; i++) {
+      const data = this.mission[i];
+      if (data.status in res) {
+        res[data.status as keyof typeof res]++;
+      }
+    }
+    return res;
+  }
+
+  create(mission: IMission) {
+    const data = fs.readFileSync("data/missions.json", "utf8");
+    const missions = JSON.parse(data) as IMission[];
+    missions.push({
+      ...mission,
+      id: (missions.length + 1).toString(),
+      status: "ACTIVE",
+      endDate: null,
+    });
+    return mission;
+  }
+
+  findAll() {
+    const data = fs.readFileSync("data/missions.json", "utf8");
+    const missions = JSON.parse(data) as IMission[];
+    return missions.map((mission) => ({
+      ...mission,
+      durationDays: mission.endDate
+        ? (new Date(mission.endDate).getTime() -
+            new Date(mission.startDate).getTime()) /
+          86400000
+        : -1,
+    }));
+  }
+
+  findOne(id: string, clearance: string = "STANDARD") {
+    const data = fs.readFileSync("data/missions.json", "utf8");
+    const missions = JSON.parse(data) as IMission[];
+    const mission = missions.find((m) => m.id === id)!;
+    if (!mission) {
+      return null;
+    } else if (mission.riskLevel == "HIGH") {
+      if (clearance == "SECRET") {
+        return {
+          ...mission,
+          targetName: "***REDACTED***",
+        };
+      } else if (clearance == "TOP_SECRET") {
+        return mission;
+      } else {
+        return null;
+      }
+    }
+    return mission;
   }
 }
